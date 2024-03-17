5ef0f7c6dfbe76fdc6fda8ef1deec01d872f2734
Sayid <sayidalijumanazarov@gmail.com>
2024-03-18 01:06:01 +0500

    ver1

---
+++ something.js
@@ -0,0 +1,20 @@
+var a =prompt("Yoshingiz Nechida")
+
+if (a <= 17) {
+    alert("Hali Yoshlik Qilasan")
+
+} else if(a <= 49) {
+    alert("San Baribir Yosh Bolasan")
+}
+
+if (a <= 58) {
+    alert("Qara Kuting Buq Ekan")
+} else if(a <= 149) {
+    alert("Tozaroq Art Kutini")
+}    
+
+else if( a => 149) {
+    alert("Chota Uzini Loh qvosanda")
+}
+
+
